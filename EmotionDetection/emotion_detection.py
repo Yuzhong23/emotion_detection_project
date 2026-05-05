5d5601370fb785c4bd072606aa75cd32240579d2
Yuzhong23 <fatursetia123@gmail.com>
2026-05-05 19:58:14 +0700

    Task 3: Implementasi format dictionary dan dominant emotion

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,42 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    
+    response = requests.post(url, json=myobj, headers=header)
+    
+    # Mengubah teks respon JSON menjadi dictionary
+    formatted_response = json.loads(response.text)
+    
+    # Mengambil skor emosi dari struktur JSON
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    anger_score = emotions['anger']
+    disgust_score = emotions['disgust']
+    fear_score = emotions['fear']
+    joy_score = emotions['joy']
+    sadness_score = emotions['sadness']
+    
+    # Mencari emosi dengan skor tertinggi (Dominant Emotion)
+    emotion_list = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score
+    }
+    dominant_emotion = max(emotion_list, key=emotion_list.get)
+    
+    # Menyusun hasil akhir sesuai format yang diminta
+    result = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
+    
+    return result
